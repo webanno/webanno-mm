9c62f88a431b4a6c57de0a569ecf542d9774437a
Steffen Remus <steremus@gmail.com>
2017-07-19 09:16:35 +0200

    update webanno master

--- webanno-webapp-exm/src/main/java/de/tudarmstadt/ukp/clarin/webanno/webapp/WicketApplication.java
+++ webanno-webapp-exm/src/main/java/de/tudarmstadt/ukp/clarin/webanno/webapp/WicketApplication.java
@@ -32,29 +32,30 @@ import de.tudarmstadt.ukp.clarin.webanno.ui.menu.MainMenuPage;
  */
 @Component("wicketApplication")
 public class WicketApplication
-extends WicketApplicationBase
+    extends WicketApplicationBase
 {
-	/**
-	 * @see org.apache.wicket.Application#getHomePage()
-	 */
-	@Override
-	public Class<? extends Page> getHomePage()
-	{
-		return MainMenuPage.class;
-	}
+    /**
+     * @see org.apache.wicket.Application#getHomePage()
+     */
+    @Override
+    public Class<? extends Page> getHomePage()
+    {
+        return MainMenuPage.class;
+    }
 
-	@Override
-	protected void init() {
-		super.init();
-		MediaResourceReference mediaresources = new MediaResourceReference();
-		Injector.get().inject(mediaresources); // manually inject springbeans since autoinjection only works for subclasses of Component
-		// mount the media resource
-		mountResource(
-				String.format("/media/${%s}/${%s}", 
-						MediaResourceStreamResource.PAGE_PARAM_PROJECT_ID, 
-						MediaResourceStreamResource.PAGE_PARAM_FILE_ID), 
-				mediaresources);
+    @Override
+    protected void init() {
+      super.init();
+             MediaResourceReference mediaresources = new MediaResourceReference();
+             Injector.get().inject(mediaresources); // manually inject springbeans since autoinjection only works for subclasses of Component
+             // mount the media resource
+             mountResource(
+                     String.format("/media/${%s}/${%s}",
+                             MediaResourceStreamResource.PAGE_PARAM_PROJECT_ID,
+                             MediaResourceStreamResource.PAGE_PARAM_FILE_ID),
+                     mediaresources);
 
 
-	}
+    }
+
 }
