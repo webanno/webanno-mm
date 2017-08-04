3075493dd4b4542ffc6cb4b2232a7c6ff7f162b5
Steffen Remus <steremus@gmail.com>
2017-08-04 17:46:55 +0200

    speaker & document details

--- webanno-plugin-exmaralda/src/main/java/de/tudarmstadt/ukp/clarin/webanno/ui/exmaralda/SpeakerDetailsWindow.java
+++ webanno-plugin-exmaralda/src/main/java/de/tudarmstadt/ukp/clarin/webanno/ui/exmaralda/SpeakerDetailsWindow.java
@@ -1,9 +1,21 @@
 package de.tudarmstadt.ukp.clarin.webanno.ui.exmaralda;
 
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.wicket.core.request.handler.IPartialPageRequestHandler;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
 
 import de.uhh.lt.webanno.exmaralda.io.TeiMetadata.Speaker;
 
@@ -46,10 +58,17 @@ public class SpeakerDetailsWindow extends ModalWindow {
 
         public SpeakerDetailsWindowContent(String id, final SpeakerDetailsWindow window) {
             super(id);
-            add(new Label("i", window.getSpeaker().i));
-            add(new Label("n", window.getSpeaker().n));
-            add(new Label("id", window.getSpeaker().id));
-            add(new Label("xmldetails", window.getSpeaker().xml_string));
+            String speaker_xml = window.getSpeaker().xml_string;
+            List<Pair<String, String>> key_value_pairs = parseXML(speaker_xml);
+            add(new ListView<Pair<String, String>>("xmlelementcontainer", key_value_pairs) {
+                private static final long serialVersionUID = 1L;
+                @Override
+                protected void populateItem(ListItem<Pair<String, String>> item) {
+                    Pair<String, String> pair = item.getModelObject();
+                    item.add(new Label("xmlelementname", pair.getKey()));
+                    item.add(new Label("xmlelementtext", pair.getValue()));
+                }                
+            });
         }
 
         /**
@@ -59,4 +78,46 @@ public class SpeakerDetailsWindow extends ModalWindow {
         
     }
     
+    public static List<Pair<String, String>> parseXML(String xml){
+        List<Pair<String, String>> result = new ArrayList<>();
+        try {
+            Reader r = new StringReader(xml);
+            SAXBuilder saxBuilder = new SAXBuilder();
+            Document document = saxBuilder.build(r);
+            Element root = document.getRootElement();
+            parseElement(root, result, "");
+            r.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    
+    private static void parseElement(Element element, List<Pair<String, String>> currentlist, String prefix){
+        String key = element.getName();
+        if("langKnown".equals(key)){
+            parseLang(element, currentlist);
+            return;
+        }
+        if("persName".equals(key))
+            key = "Name";
+        if("birth".equals(key))
+            prefix = "Birth ";
+        String val = element.getTextNormalize();    
+        if(!StringUtils.isEmpty(val))
+            currentlist.add(Pair.of(String.format("%s%s", prefix, StringUtils.capitalize(key)), val));
+        for(Element child : element.getChildren())
+            parseElement(child, currentlist, prefix);
+    }
+    
+    private static void parseLang(Element langKnown, List<Pair<String, String>> currentlist){
+        String key = String.format("Language (%s)", langKnown.getAttributeValue("level"));
+        String val = String.format("%s (%s)", langKnown.getTextNormalize(),  langKnown.getAttributeValue("tag"));
+        currentlist.add(Pair.of(StringUtils.capitalize(key), val));
+        
+    }
+    
+    
+
 }
