f63c75f879027d493ab5009327a3d793c87742bd
Steffen Remus <steremus@gmail.com>
2017-09-04 18:07:10 +0200

    new build process

--- build.sh
+++ build.sh
@@ -4,13 +4,12 @@ set -ex
 
 if [ -z "$1" ]; then
 	mvn clean install -U -DskipTests -Dcheckstyle.skip=true -Drat.skip=true -f webanno/pom.xml
-	mvn clean install -U -DskipTests -Dcheckstyle.skip=true -Drat.skip=true -f webanno-plugin-exmaralda/pom.xml
-	mvn clean package -U -DskipTests -Dcheckstyle.skip=true -Drat.skip=true -f webanno-webapp-exm/pom.xml
+	mvn clean install -U -DskipTests -Dcheckstyle.skip=true -Drat.skip=true -f pom.xml
 else
 	if [ "$1" = "docker" ]; then
     set +x
     echo -n "Password: "
     read -s p
-		mvn -f webanno-webapp-exm/pom.xml -Pdocker docker:build docker:push -Ddocker.username=remstef -Ddocker.password=$p
+		mvn -f pom.xml -Pdocker docker:build docker:push -Ddocker.username=remstef -Ddocker.password=$p
 	fi
 fi
