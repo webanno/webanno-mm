d4c858f7ff892d78a66f0c0211e8cd5c75e386e6
Steffen Remus <steremus@gmail.com>
2018-09-28 15:58:34 +0200

    build options

--- build.sh
+++ build.sh
@@ -3,13 +3,12 @@
 set -ex
 
 if [ -z "$1" ]; then
-	mvn clean install -U -DskipTests -Dcheckstyle.skip=true -Drat.skip=true -f webanno/pom.xml
-	mvn clean install -U -DskipTests -Dcheckstyle.skip=true -Drat.skip=true -f pom.xml
-else
-	if [ "$1" = "docker" ]; then
-    set +x
-    echo -n "Password: "
-    read -s p
-		mvn -f pom.xml -Pdocker docker:build docker:push -Ddocker.username=remstef -Ddocker.password=$p
-	fi
+  mvn clean install -U -DskipTests -Dcheckstyle.skip=true -Drat.skip=true -f pom.xml
+elif [ "$1" = "webanno" ]; then
+  mvn clean install -U -DskipTests -Dcheckstyle.skip=true -Drat.skip=true -f webanno/pom.xml
+elif [ "$1" = "docker" ]; then
+  set +x
+  echo -n "Password: "
+  read -s p
+  mvn -f pom.xml -Pdocker docker:build docker:push -Ddocker.image.name=webanno/webanno-mm -Ddocker.username=remstef -Ddocker.password=$p
 fi
